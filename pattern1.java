facd1f84bc8f1861d77d05a8134086f441e8a93a
Subhash Kumar <85139394+subhash1e@users.noreply.github.com>
2022-01-05 23:33:52 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,25 @@
+class Tester {
+	public static void main(String[] args) {
+		// Implement your code here 
+		for(int i = 0; i<5;i++){
+		    for(int j = 0; j<5-i;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+/*
+
+
+output:
+
+*****
+****
+***
+**
+*
+
+
+
+*/
